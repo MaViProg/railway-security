d2348b7bf6f1b8712f294534499aefd295b4ba4e
MaViProg <viktrija-iknnikva@yandex.ru>
2023-06-08 13:10:21 +0300

    add comments fix version

--- src/main/java/com/mavi/restrailwaysecurity/conroller/StationModelController.java
+++ src/main/java/com/mavi/restrailwaysecurity/conroller/StationModelController.java
@@ -17,35 +17,76 @@ public class StationModelController {
         this.stationModelRepository = stationModelRepository;
     }
 
+    /**
+     * getAllStationModels()
+     * GET http://localhost:8084/api/station-models
+     *
+     * @return
+     */
     @GetMapping
     public List<StationModel> getAllStationModels() {
         return stationModelRepository.findAll();
     }
 
+    /**
+     * getStationModelById()
+     * GET http://localhost:8084/api/station-models/id
+     *
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     public StationModel getStationModelById(@PathVariable Long id) {
         return stationModelRepository.findById(id).orElseThrow();
     }
 
+    /**
+     * createStationModel()
+     * POST http://localhost:8084/api/station-models
+     *
+     * @param stationModel
+     * @return
+     */
     @PostMapping
     public StationModel createStationModel(@RequestBody StationModel stationModel) {
         return stationModelRepository.save(stationModel);
     }
 
+    /**
+     * updateStationModel()
+     * PUT http://localhost:8084/api/station-models/id
+     *
+     * @param id
+     * @param stationModel
+     * @return
+     */
     @PutMapping("/{id}")
     public StationModel updateStationModel(@PathVariable Long id, @RequestBody StationModel stationModel) {
         stationModel.setId(id);
         return stationModelRepository.save(stationModel);
     }
 
+    /**
+     * deleteStationModel()
+     * DELETE http://localhost:8084/api/station-models/id
+     *
+     * @param id
+     * @return
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteStationModel(@PathVariable Long id) {
         stationModelRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
+    /**
+     * deleteAllStationModel()
+     * DELETE http://localhost:8084/api/station-models
+     *
+     * @return
+     */
     @DeleteMapping
-    public ResponseEntity<Void> deleteAllWagons() {
+    public ResponseEntity<Void> deleteAllStationModel() {
         stationModelRepository.deleteAll();
         return ResponseEntity.noContent().build();
     }
