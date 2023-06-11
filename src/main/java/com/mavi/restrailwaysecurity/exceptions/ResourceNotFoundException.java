d675719a2b7817ac040549f7816bd66e807ac274
MaViProg <viktrija-iknnikva@yandex.ru>
2023-06-11 12:00:01 +0300

    add exceptions

---
+++ src/main/java/com/mavi/restrailwaysecurity/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mavi.restrailwaysecurity.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
